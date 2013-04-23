fc428c2cee1ec511a001f7a4280bb371cd66243d
Brendan McGarrett <bmcgarrett@gmail.com>
2013-04-22 20:09:50 -0400

    Update page with twitter bootstrap

--- index.php
+++ index.php
@@ -5,11 +5,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 </head>
 <body>
-<h1>Twitter Bootstrap</h1>
-<script src="http://code.jquery.com/jquery.js"></script>
-<script src="js/bootstrap.min.js"></script>
+<div class='container'>
+    <h1>TWITTER BOOTSTRAP TUTORIAL</h1>
+    <div class='navbar navbar-inverse'>
+        <div class='navbar-inner nav-collapse' style="height: auto;">
+            <ul class="nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Page One</a></li>
+                <li><a href="#">Page Two</a></li>
+            </ul>
+        </div>
+    </div>
+    <div id='content' class='row-fluid'>
+        <div class='span9 main'>
+            <h2>Main Content Section</h2>
+        </div>
+        <div class='span3 sidebar'>
+            <h2>Sidebar</h2>
+            <ul class="nav nav-tabs nav-stacked">
+                <li><a href='#'>Another Link 1</a></li>
+                <li><a href='#'>Another Link 2</a></li>
+                <li><a href='#'>Another Link 3</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
 </body>
 </html>
 
