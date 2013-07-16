84c1ead79ca3a94f85dd0fc63caf6a0500b62b5b
Brendan McGarrett <bmcgarrett@gmail.com>
2013-07-16 15:26:31 -0400

    Removed secondary nav from home page

--- index.php
+++ index.php
@@ -3,14 +3,6 @@
     <div class='span9 main'>
         <h2>Change Log</h2>
     </div>
-    <div class='span3 sidebar'>
-        <h2>Secondary Nav</h2>
-        <ul class="nav nav-tabs nav-stacked">
-            <li><a href='#'>Test Link 1</a></li>
-            <li><a href='#'>Test Link 2</a></li>
-            <li><a href='#'>Test Link 3</a></li>
-        </ul>
-    </div>
 </div>
 <?php
 include_once('log4php/logger.php');
