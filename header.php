990f4d7d0c98473ed5d78b621d142f7eb250699e
Brendan McGarrett <bmcgarrett@gmail.com>
2013-07-16 14:55:02 -0400

    Updated body tag

--- header.php
+++ header.php
@@ -13,6 +13,7 @@
 <div class='navbar navbar-fixed-top'>
     <div class='navbar-inner'>
        <div class="container">
+       <a class="brand" href="../">PHP Bootstrap</a>
             <div class="nav-collapse collapse" id="main-menu">
                 <ul class="nav" id="main-menu-left">
                     <li class="active"><a href="index.php"><i class='icon-home'></i> Home</a></li>
