6de4e028a88f97dd8724b93dcf50b06c449bdf88
Brendan McGarrett <bmcgarrett@gmail.com>
2013-07-16 15:38:24 -0400

    Working selectors for nav menu

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
        <a class="brand" href="../">PHP Bootstrap</a>
             <div class="nav-collapse collapse" id="main-menu">
                 <ul class="nav" id="main-menu-left">
-                    <li class="active"><a href="index.php"><i class='icon-home'></i> Home</a></li>
+                    <li class="active"><a href="/index.php"><i class='icon-home'></i> Home</a></li>
                     <li><a href="/mySqlTests.php"><i class='icon-hdd'></i> MySQL</a></li>
                 </ul>
             </div>
