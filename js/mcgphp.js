f2ff5862992f6cbc73211657e0f1fe9c0938418a
Brendan McGarrett <bmcgarrett@gmail.com>
2013-07-16 15:31:57 -0400

    Added javascript file for jquery

--- js/mcgphp.js
+++ js/mcgphp.js
@@ -1,7 +1,3 @@
 $(document).ready(function() {
-	$('ul.nav > li').click(function (e) {
-            e.preventDefault();
-            $('ul.nav > li').removeClass('active');
-            $(this).addClass('active');                
-        }); 
+	$('ul.nav > li > a[href="' + document.location.pathname + '"]').parent().addClass('active');
 })
