ba358a2c997b9592ec7fef9f8e309c3de68cf4ed
Brendan McGarrett <bmcgarrett@gmail.com>
2013-07-16 20:33:40 -0400

    Changed table class

--- js/mcgphp.js
+++ js/mcgphp.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
 	$('ul.nav > li > a[href="' + document.location.pathname + '"]').parent().addClass('active');
 
     $('#saveBtnAddBook').on("click", function(){
-        e.preventDefault();
         var myBookTitle = $('#bookTitleInput').val();
         var myBookAuthor = $('#bookAuthorInput').val();
         $.post("/mySqlTests.php", { bTitle: myBookTitle, bAuthor: myBookAuthor });
