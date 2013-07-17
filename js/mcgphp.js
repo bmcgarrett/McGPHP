5a39777c96a653182d49c23e0ca1b29e71470d17
Brendan McGarrett <bmcgarrett@gmail.com>
2013-07-16 21:17:29 -0400

    Changed table class

--- js/mcgphp.js
+++ js/mcgphp.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$('.navbar li').removeClass('active');
 	$('ul.nav > li > a[href="' + document.location.pathname + '"]').parent().addClass('active');
 
-    $('#saveBtnAddBook').on("click", function(){
+    $('#saveBtnAddBook').on('click', function(){
         var myBookTitle = $('#bookTitleInput').val();
         var myBookAuthor = $('#bookAuthorInput').val();
         $.post("/mySqlTests.php", { bTitle: myBookTitle, bAuthor: myBookAuthor }).done(function () {
@@ -10,7 +10,7 @@ $(document).ready(function() {
         });
     });
 
-    $(document).on("click","#deleteBookRow",function(){
+    $(document).on('click','#deleteBookRow',function(){
         var bookTitleField = $(this).parent().parent().find('#bookTitleField').text();
         var bookAuthorField = $(this).parent().parent().find('#bookAuthorField').text();
         alert(bookTitleField + bookAuthorField);
