b379a7b71e3406e2e2e81e008111fedc08bc6694
Brendan McGarrett <bmcgarrett@gmail.com>
2013-07-16 20:29:20 -0400

    Changed table class

--- js/mcgphp.js
+++ js/mcgphp.js
@@ -7,6 +7,6 @@ $(document).ready(function() {
         var myBookTitle = $('#bookTitleInput').val();
         var myBookAuthor = $('#bookAuthorInput').val();
         $.post("/mySqlTests.php", { bTitle: myBookTitle, bAuthor: myBookAuthor });
-        location.href = location.href;
+        $.get("/mySqlTests.php");
     });
 });
