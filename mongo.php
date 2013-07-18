436fbfd7f6779adee5ad8f4212184c78b9e47786
Brendan McGarrett <bmcgarrett@gmail.com>
2013-07-18 12:32:37 -0400

    Updates to mongo page

--- mongo.php
+++ mongo.php
@@ -2,30 +2,41 @@
 
 try {
 
-$connection_url = getenv("CUSTOMCONNSTR_mongo");
-$collection_name = "learn";
+    $connection_url = getenv("CUSTOMCONNSTR_mongo");
+    $collection_name = "learn";
 
 // create the mongo connection object
-$m = new Mongo($connection_url);
+    $m = new Mongo($connection_url);
 
 // extract the DB name from the connection path
-$url = parse_url($connection_url);
-$db_name = preg_replace('/\/(.*)/', '$1', $url['path']);
+    $url = parse_url($connection_url);
+    $db_name = preg_replace('/\/(.*)/', '$1', $url['path']);
 
 // use the database we connected to
-$db = $m->selectDB($db_name);
+    $db = $m->selectDB($db_name);
 
-$collection = $db->selectCollection($collection_name);
+    $collection = $db->selectCollection($collection_name);
 
-$cursor = $collection->find();
+    $cursor = $collection->find();
 
-foreach ( $cursor as $doc ){
-    var_dump($doc);
-}
+    echo "<h1>Mongo Books Database</h1>";
+    echo "<table class='table table-striped table-bordered table-hover'>";
+    echo "<thead><tr><th id='deleteColumnHeading'></th><th>ID</th><th>Title</th><th>Author</th><th id='editColumnHeading'></th></tr></thead>";
+    echo "<tbody>";
+
+    $bookCount = 1;
+    foreach ( $cursor as $doc ){
+        echo "<tr rowID='" . $doc['_id'] . "'><td><a id='deleteBookRow' class='btn btn-danger' href='#'>Delete</a><td>" . $bookCount . "</td><td id='bookTitleField'>" . $doc['title'] . "</td><td id='bookAuthorField'>" . $doc["author"] . "</td><td><a id='editBookBtn' data-toggle='modal' role='button' class='btn' href='#editBookModal'>Edit</a></td></tr>";
+        $bookCount++;
+    }
+
+    echo "</tbody>";
+    echo "</table>";
+    echo "<a class='btn' href='#addBookModal' data-toggle='modal' role='button'>Add Book</a>";
 
 
 // disconnect from server
-$m->close();
+    $m->close();
 } catch ( MongoConnectionException $e ) {
     die('Error connecting to MongoDB server');
 } catch ( MongoException $e ) {
