0c16dc8da5280a4c8016ccab757ace0703cc9d39
Brendan McGarrett <bmcgarrett@gmail.com>
2013-07-18 11:45:53 -0400

    Updates to mongo page

--- mongo.php
+++ mongo.php
@@ -3,6 +3,7 @@
 try {
 
 $connection_url = getenv("CUSTOMCONNSTR_mongo");
+$collection_name = "learn";
 
 // create the mongo connection object
 $m = new Mongo($connection_url);
@@ -14,33 +15,14 @@ $db_name = preg_replace('/\/(.*)/', '$1', $url['path']);
 // use the database we connected to
 $db = $m->selectDB($db_name);
 
-echo "<h2>Collections</h2>";
-echo "<ul>";
+$collection = $db->selectCollection($collection_name);
 
-// print out list of collections
-$cursor = $db->listCollections();
-$collection_name = "";
-foreach( $cursor as $doc ) {
-    echo "<li>" .  $doc->getName() . "</li>";
-    $collection_name = $doc->getName();
+$cursor = $collection->find();
+
+foreach ( $cursor as $doc ){
+    var_dump($doc);
 }
-echo "</ul>";
 
-// print out last collection
-if ( $collection_name != "" ) {
-    $collection = $db->selectCollection($collection_name);
-    echo "<h2>Documents in ${collection_name}</h2>";
-
-    // only print out the first 5 docs
-    $cursor = $collection->find();
-    $cursor->limit(5);
-    echo $cursor->count() . ' document(s) found. <br/>';
-    foreach( $cursor as $doc ) {
-        echo "<pre>";
-        var_dump($doc);
-        echo "</pre>";
-    }
-}
 
 // disconnect from server
 $m->close();
@@ -51,5 +33,5 @@ $m->close();
 } catch ( Exception $e ) {
     die('Error: ' . $e->getMessage());
 }
-?>
+
 include_once('footer.php'); ?>
