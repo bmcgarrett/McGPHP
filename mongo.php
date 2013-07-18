562d2680fb07486c2fc046740bcb2ac6ec29aeb1
Brendan McGarrett <bmcgarrett@gmail.com>
2013-07-18 08:22:06 -0400

    Updates to mongo page

--- mongo.php
+++ mongo.php
@@ -1,5 +1,26 @@
 <?php include_once('header.php'); ?>
 
+<?php
+
+$conn_str = getenv("CUSTOMCONNSTR_mongo");
+
+$m = new Mongo($conn_str);
+
+$url = parse_url($connection_url);
+$db_name = preg_replace('/\/(.*)/', '$1', $url['path']);
+
+$db = $m->selectDB($db_name);
+
+$cursor = $db->listCollections();
+$collection_name = "";
+foreach( $cursor as $doc ) {
+    echo "<li>" .  $doc->getName() . "</li>";
+    $collection_name = $doc->getName();
+}
+echo "</ul>";
+
+?>
+
 
 
 <?php include_once('footer.php'); ?>
